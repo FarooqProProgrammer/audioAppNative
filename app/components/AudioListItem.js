323e686189c294d4ca29e3c6265d6a46710edf87
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2023-02-16 21:39:55 -0800

    Updated

--- app/components/AudioListItem.js
+++ app/components/AudioListItem.js
@@ -7,13 +7,16 @@ import { yellow100 } from 'react-native-paper/lib/typescript/styles/themes/v2/co
 export class AudioListItem extends Component {
     render() {
         return (
+            <>
             <View style={styles.container}>
                 <View style={styles.leftContainer}>
                     <View style={styles.thumbnail}>
                         <Text style={styles.thumbnailText}>A</Text>
                     </View>
                     <View style={styles.titleContainer}>
-                        <Text style={styles.title}>A</Text>
+                        <Text numberOfLines={1} style={styles.title}>A</Text>
+                        <Text numberOfLines={1} style={styles.timeText}>A</Text>
+                        03:59
                     </View>
 
                 </View>
@@ -21,6 +24,8 @@ export class AudioListItem extends Component {
                     <Entypo name="dots-three-vertical" size={24} color={color.FONT_MEDIUM} />
                 </View>
             </View>
+                <View  style={styles.separator}/>
+            </>
         )
     }
 }
@@ -64,6 +69,16 @@ const styles = StyleSheet.create({
     title :{
         fontSize:16,
         color:color.FONT
+    },separator:{
+        width:width - 80,
+        backgroundColor:"#333",
+        opacity:0.3,
+        alignSelf:'center',
+        marginTop:10,
+    },
+    timeText:{
+        fontSize:14,
+        color:color.FONT_LIGHT,
     }
 
 })
