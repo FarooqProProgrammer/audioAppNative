4c39e3bf3fafd9365fb179cf6ba3eb3836453648
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2023-02-16 22:37:48 -0800

    Updated

--- App.js
+++ App.js
@@ -1,15 +1,23 @@
-import { NavigationContainer } from '@react-navigation/native'
-import React from 'react'
-import AppNavigator from './app/navigation/AppNavigator'
-import AudioProvider from './app/context/AudioProvider'
+import React from 'react';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import AppNavigator from './app/navigation/AppNavigator';
+import AudioProvider from './app/context/AudioProvider';
+import color from './app/misc/color';
+
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: color.APP_BG,
+  },
+};
 
 export default function App() {
   return (
     <AudioProvider>
-      <NavigationContainer>
+      <NavigationContainer theme={MyTheme}>
         <AppNavigator />
       </NavigationContainer>
     </AudioProvider>
-  )
+  );
 }
-
--- app/misc/color.js
+++ app/misc/color.js
@@ -1,9 +1,9 @@
 export default {
-    APP_BG : '#FFF',
-    FONT: '#303D49',
-    FONT_MEDIUM: '#636363',
-    FONT_LIGHT: '#B6B8B9',
-    MODAL_BG: 'RGBA(0,0,0.2)',
-    ACTIVE_BG: '#5252AD',
-    ACTIVE_FONT: '#FFF'
-}
+  APP_BG: '#fff',
+  FONT: '#303d49',
+  FONT_MEDIUM: '#636363',
+  FONT_LIGHT: '#b6b8b9',
+  MODAL_BG: 'rgba(0, 0, 0, 0.2)',
+  ACTIVE_BG: '#5252ad',
+  ACTIVE_FONT: '#fff',
+};
